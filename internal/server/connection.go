42f3b02bc66546348da23f5a92d3662560090bfa
H.W.K.Aravinda <118424430+Aravinda-HWK@users.noreply.github.com>
2025-10-17 11:23:22 +0530

    feat: implement EXPUNGE command handling with associated tests

--- internal/server/connection.go
+++ internal/server/connection.go
@@ -74,6 +74,8 @@ func handleClient(s *IMAPServer, conn net.Conn, state *models.ClientState) {
 			s.handleCheck(conn, tag, state)
 		case "CLOSE":
 			s.handleClose(conn, tag, state)
+		case "EXPUNGE":
+			s.handleExpunge(conn, tag, state)
 		case "SUBSCRIBE":
 			s.handleSubscribe(conn, tag, parts, state)
 		case "UNSUBSCRIBE":
