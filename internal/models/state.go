8a098cec7b87fd0430cc17f075640ec1bfcd8521
H.W.K.Aravinda <118424430+Aravinda-HWK@users.noreply.github.com>
2025-11-06 16:49:33 +0530

    feat: add role mailbox support to ClientState model

--- internal/models/state.go
+++ internal/models/state.go
@@ -15,4 +15,8 @@ type ClientState struct {
 	LastRecentCount    int    // Last known recent (unseen) message count
 	UIDValidity        int64  // UID validity for selected mailbox
 	UIDNext            int64  // Next UID for selected mailbox
+	// Role mailbox support
+	RoleMailboxIDs     []int64  // Database IDs of role mailboxes assigned to this user
+	SelectedRoleMailboxID int64 // Database ID of selected role mailbox (0 if not a role mailbox)
+	IsRoleMailbox      bool     // True if currently browsing a role mailbox
 }
